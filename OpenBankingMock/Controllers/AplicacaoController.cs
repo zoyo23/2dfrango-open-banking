4a1d9f0f216c868aa4203bac3cd83e547fe097e5
phmxavier <phmxavier@gmail.com>
2020-05-02 18:15:47 -0300

    Inclusão do código inicial da tela de login

---
+++ OpenBankingMock/Controllers/AplicacaoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenBankingMock.Controllers
+{
+    /*
+     * Este controller está aqui apenas para simular a chamada da aplicação. Existe apenas em tempo de desenvolvimento
+     */
+    [Route("[controller]")]
+    [ApiController]
+    public class AplicacaoController : ControllerBase
+    {
+        public JsonResult Get(string token)
+        {
+            return new JsonResult(new { token = token });
+        }
+    }
+}
---
+++ OpenBankingMock/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OpenBankingMock.Models;
+
+namespace OpenBankingMock.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string cpf, string senha)
+        {
+            // Buscar se o usuário existe no banco de dados
+            // Gerar o token de autorização do cliente
+            return Redirect(@$"/aplicacao?token={cpf}");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
